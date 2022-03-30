3b3ad4822145b8e7b7a99a2ea54e6bd4cb1af023
NatashaBazunova <natashaiosifova@mail.ru>
2022-03-30 18:33:32 +0500

    Initial commit

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,69 @@
+package ru.netology.radio;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int maxVolume = 10;
+    private int minVolume = 0;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+    public void nextStation() {
+        if (currentStation < 9) {
+           currentStation = currentStation + 1;
+        }
+        if (currentStation >= 9){
+            this.currentStation = minStation;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        }
+        if (currentStation == 0) {
+            this.currentStation = maxStation;
+        }
+    }
+    public int setCurrentStation(int currentStation) {
+        if (currentStation >=0 & currentStation <=9) {
+            this.currentStation = currentStation;
+        }
+        else {
+            this.currentStation = minStation;
+        }
+        return this.currentStation;
+    }
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+        if (currentVolume == 10) {
+            this.currentVolume = maxVolume;
+        }
+    }
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+        if (currentVolume == 0) {
+            this.currentVolume = minVolume;
+        }
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume >= 0 & currentVolume <= 10) {
+            this.currentVolume = currentVolume;
+        }
+        else {
+            this.currentVolume = maxVolume;
+        }
+    }
+}
